f9e1113186eba8b9dc5e3ed7ea0c1fa66ba87b80
Enric Moreu <enric.moreu.filella@gmail.com>
2019-12-04 13:13:30 +0000

    Simple mask

--- src/app.py
+++ src/app.py
@@ -7,14 +7,29 @@ setup_logger()
 import numpy as np
 import cv2
 import random
-from google.colab.patches import cv2_imshow
+# from google.colab.patches import cv2_imshow
 
 # import some common detectron2 utilities
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
-from detectron2.data import MetadataCatalog'
+from detectron2.data import MetadataCatalog
 
+torch.cuda.set_device(0)
 
-# im = cv2.imread("./input.jpg")
-# cv2_imshow(im)
+im = cv2.imread("../data/frames/out1080.mp4-00000.jpg")
+
+cfg = get_cfg()
+# add project-specific config (e.g., TensorMask) if you're not running a model in detectron2's core library
+cfg.merge_from_file("/detectron2_repo/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+# Find a model from detectron2's model zoo. You can either use the https://dl.fbaipublicfiles.... url, or use the following shorthand
+cfg.MODEL.WEIGHTS = "detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl"
+predictor = DefaultPredictor(cfg)
+outputs = predictor(im)
+
+###
+
+v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+cv2.imwrite('out.png', v.get_image()[:, :, ::-1])
