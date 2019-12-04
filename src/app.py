2aa92e48a2146f9c0f6b3da9da6b7af19e3bcae6
Enric Moreu <enric.moreu.filella@gmail.com>
2019-12-04 12:38:59 +0000

    Dockerize environment

---
+++ src/app.py
@@ -0,0 +1,20 @@
+import torch, torchvision
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import cv2
+import random
+from google.colab.patches import cv2_imshow
+
+# import some common detectron2 utilities
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog'
+
+
+# im = cv2.imread("./input.jpg")
+# cv2_imshow(im)
